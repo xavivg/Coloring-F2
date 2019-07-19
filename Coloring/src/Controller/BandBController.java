b9e1fca93f057c23e6a26a2b07f9ef18d3152ce8
xavivg <xavivilardellgras@gmail.com>
2019-07-19 18:45:07 +0200

    Comparador cambiado

--- Coloring/src/Controller/BandBController.java
+++ Coloring/src/Controller/BandBController.java
@@ -11,9 +11,9 @@ import static model.Main.xMejor;
 public class BandBController {
 
     public static void branchAndBound(Conf conf, int k) throws InterruptedException {
-        PriorityQueue nodosVivos = new PriorityQueue(new Comparator<Conf>() {
+        PriorityQueue<Configuracion> nodosVivos = new PriorityQueue(new Comparator<Configuracion>() {
             @Override
-            public int compare(Conf o1, Conf o2) {
+            public int compare(Configuracion o1, Configuracion o2) {
                 return o1.getNota() - o2.getNota();
             }
         });
@@ -23,7 +23,7 @@ public class BandBController {
         Conf[] hijos = new Conf[MAX_COLORS];
         hijos = expande(x);
         while (nodosVivos.isEmpty()){
-           //Configuracion = nodosVivos.peek();
+           x = nodosVivos.poll();
         }
         nodosVivos.remove(x);
     }
